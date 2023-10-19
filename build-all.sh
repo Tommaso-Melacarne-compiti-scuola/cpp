d0e1f265f45da61b353e54d5b735a5125e3334de
PolpOnline <git@t0mmy.anonaddy.com>
2023-10-19 19:42:24 +0200

    Ignore building template.cpp

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Define an array of cpp filenames to ignore (without .cpp extension)
+IGNORE_FILES=("template")
+
+export IGNORE_FILES BashArrayToString="${IGNORE_FILES[*]}"
+
 find . -name "*.cpp" -type f -exec bash -c '
   for file do
     filename=$(basename "$file" ".cpp")
     dir=$(dirname "$file")
+
+    # Ignore files in the IGNORE_FILES array
+    if grep -oqwF "$filename" <<<"${BashArrayToString}"; then
+      continue
+    fi
+
     pushd "$dir" > /dev/null
     g++ -Wall -Wextra -pedantic "$filename.cpp" -o "$filename" && echo "Built $filename in $dir"
     popd > /dev/null
