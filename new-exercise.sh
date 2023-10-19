5599019f053e8cef3c9602ef24fa06661f49e8c0
PolpOnline <git@t0mmy.anonaddy.com>
2023-10-19 19:21:44 +0200

    Change folder structure and file creation method in new-exercise.sh
    Modified the script to create a new exercise. The file now creates a new directory inside the 'assignments' folder, allowing a more structured approach for exercise classification. Furthermore, rather than creating an empty .cpp file, a template .cpp file is copied into the new exercise directory.
    
    Commit message written by Gippity :)

--- new-exercise.sh
+++ new-exercise.sh
@@ -8,7 +8,7 @@ exercise_nunber=$1
 exercise_name=$2
 
 # create directory
-mkdir -p "$exercise_name"
+mkdir -p ./assignments/"$exercise_nunber"/"$exercise_name"
 
 # create file
-touch "$exercise_name"/es_"$exercise_nunber"_"$last_name"_"$exercise_name".cpp
+cp ./template.cpp ./assignments/"$exercise_nunber"/"$exercise_name"/es_"$exercise_nunber"_"$last_name"_"$exercise_name".cpp
