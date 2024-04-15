148cee57af3015b9203324a35763e4c56392224e
PolpOnline <git@t0mmy.anonaddy.com>
2024-04-15 19:47:51 +0200

    Start of assignment 61

---
+++ assignments/61/studenti/es_61_Melacarne_studenti.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <array>
+
+using namespace std;
+
+constexpr int MAX_VOTE = 10;
+constexpr int STUDENT_VOTES = 5;
+constexpr int STUDENTS = 10;
+constexpr int MAX_NAME_LENGTH = 10;
+constexpr int PRECISON_FACTOR = 100;
+
+const array<string, 8> SUBJECTS = {"Matematica", "Fisica", "Chimica", "Biologia", "Storia", "Geografia", "Letteratura",
+                                   "Arte"};
+
+// A vote with a value
+typedef struct {
+    float value;
+} Vote;
+
+// A subject with a name and a vector of votes
+typedef struct {
+    string name;
+    vector<Vote> votes;
+} Subject;
+
+// A student with a name, a surname and a vector of subjects
+typedef struct {
+    string name;
+    string surname;
+    vector<Subject> subjects;
+} Student;
+
+// Returns a random vote with a maximum value of maxValue
+Vote getRandomVote(float maxValue) {
+    Vote vote{
+            .value = (float) (rand() % (int) (maxValue * PRECISON_FACTOR) / (float) PRECISON_FACTOR)
+    };
+    return vote;
+}
+
+// Returns a vector of n random votes with a maximum value of maxValue
+vector<Vote> getRandomVotes(int n, float maxValue) {
+    vector<Vote> votes;
+    votes.reserve(n);
+    for (int i = 0; i < n; i++) {
+        votes.push_back(getRandomVote(maxValue));
+    }
+    return votes;
+}
+
+// Returns a subject with a specific name, n random votes and a maximum value of maxValue
+Subject getRandomSubject(string name, int nVotes, float maxValue) {
+    Subject subject{
+            .name = std::move(name),
+            .votes = getRandomVotes(nVotes, maxValue)
+    };
+    return subject;
+}
+
+// Returns a vector of subjects with random n votes in each subject, with a maximum value of maxValue
+vector<Subject> getRandomSubjects(int nVotes, float maxValue) {
+    vector<Subject> subjects;
+
+    subjects.reserve(SUBJECTS.size());
+
+    for (const string& subject: SUBJECTS) {
+        subjects.push_back(getRandomSubject(subject, nVotes, maxValue));
+    }
+
+    return subjects;
+}
+
+// Returns a random string of length n
+string getRandomString(int length) {
+    constexpr int ALPHABET_START = 26;
+    constexpr int ALPHABET_END = 97;
+
+    string str;
+    str.reserve(length);
+    for (int i = 0; i < length; i++) {
+        str.push_back((char) (rand() % ALPHABET_START + ALPHABET_END));
+    }
+    return str;
+}
+
+// Returns a vector of n random students
+vector<Student> getRandomStudent(int n) {
+    vector<Student> students;
+    students.reserve(n);
+    for (int i = 0; i < n; i++) {
+        Student student{
+                .name = getRandomString(MAX_NAME_LENGTH),
+                .surname = getRandomString(MAX_NAME_LENGTH),
+                .subjects = getRandomSubjects(STUDENT_VOTES, MAX_VOTE)
+        };
+
+        students.push_back(student);
+    }
+    return students;
+}
+
+// Returns the average of the votes in a subject
+float getSubjectAverage(const Subject &subject) {
+    float sum = 0;
+    for (const auto &vote: subject.votes) {
+        sum += vote.value;
+    }
+    return sum / (float) subject.votes.size();
+}
+
+// Returns the average of the votes of a student
+float getStudentAverage(const Student &student) {
+    float sum = 0;
+    for (const auto &subject: student.subjects) {
+        sum += getSubjectAverage(subject);
+    }
+    return sum / (float) student.subjects.size();
+}
+
+// Returns the student with the highest average
+Student getStudentWithMaxAverage(const vector<Student> &students) {
+    Student maxStudent = students[0];
+    float maxAverage = getStudentAverage(maxStudent);
+
+    for (const auto &student: students) {
+        float average = getStudentAverage(student);
+        if (average > maxAverage) {
+            maxAverage = average;
+            maxStudent = student;
+        }
+    }
+
+    return maxStudent;
+}
+
+// Prints all the student's  information
+void printStudent(const Student &student) {
+    cout << "Nome: " << student.name << endl;
+    cout << "Cognome: " << student.surname << endl;
+    for (const auto &subject: student.subjects) {
+        cout << "Materia: " << subject.name << endl;
+        cout << "Voti: ";
+        for (const auto &vote: subject.votes) {
+            cout << vote.value << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<Student> students = getRandomStudent(STUDENTS);
+
+    Student maxStudent = getStudentWithMaxAverage(students);
+
+    cout << "Studente con la media più alta: " << endl;
+    printStudent(maxStudent);
+
+    return 0;
+}
